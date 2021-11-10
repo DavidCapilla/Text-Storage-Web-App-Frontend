25ac951f76bfea8e19a870c4684d8a6cd502c6fb
David Capilla <dcapillag@gmail.com>
2021-11-10 17:44:03 +0100

    dcg | 001 | Format code and fix test.

--- src/components/account/Login.test.js
+++ src/components/account/Login.test.js
@@ -1,71 +1,67 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import axios from "axios";
 import { act } from "react-dom/test-utils";
 import Login from "./Login";
 
-jest.mock('axios');
+jest.mock("axios");
 
 test("Display error when sign in button is clicked with empty username.", () => {
+  const { getByTestId } = render(<Login />);
 
-    const { getByTestId } = render(<Login />);
+  let usernameHelperText = document.querySelector("[id=username-helper-text]");
+  expect(usernameHelperText).not.toBeInTheDocument();
 
-    let usernameHelperText = document.querySelector("[id=username-helper-text]");
-    expect(usernameHelperText).not.toBeInTheDocument();
+  fireEvent.click(getByTestId("sign-in-button"));
 
-    fireEvent.click(getByTestId("sign-in-button"));
-
-    usernameHelperText = document.querySelector("[id=username-helper-text]");
-    expect(usernameHelperText).toBeInTheDocument();
-    expect(usernameHelperText.textContent).toBe("Username cannot be empty");
+  usernameHelperText = document.querySelector("[id=username-helper-text]");
+  expect(usernameHelperText).toBeInTheDocument();
+  expect(usernameHelperText.textContent).toBe("Username cannot be empty");
 });
 
 test("Display error when sign in button is clicked with empty password.", () => {
+  const { getByTestId } = render(<Login />);
 
-    const { getByTestId } = render(<Login />);
+  let passwordHelperText = document.querySelector("[id=password-helper-text]");
+  expect(passwordHelperText).not.toBeInTheDocument();
 
-    let passwordHelperText = document.querySelector("[id=password-helper-text]");
-    expect(passwordHelperText).not.toBeInTheDocument();
+  fireEvent.click(getByTestId("sign-in-button"));
 
-    fireEvent.click(getByTestId("sign-in-button"));
-
-    passwordHelperText = document.querySelector("[id=password-helper-text]");
-    expect(passwordHelperText).toBeInTheDocument();
-    expect(passwordHelperText.textContent).toBe("Password cannot be empty");
+  passwordHelperText = document.querySelector("[id=password-helper-text]");
+  expect(passwordHelperText).toBeInTheDocument();
+  expect(passwordHelperText.textContent).toBe("Password cannot be empty");
 });
 
-test("Display error alert when the user cannot be authenticated.", () => {
+test("Display error alert when the user cannot be authenticated.", async () => {
+  const { getByTestId } = render(<Login />);
 
-    const { getByTestId } = render(<Login />);
+  axios.get.mockImplementationOnce(() =>
+    Promise.reject({ response: { status: 401 } })
+  );
 
-    const response = { status: 401 };
-    axios.get.mockRejectedValueOnce(new Error({ response: { status: 401 } }));
+  let username = document.querySelector("[id=username]");
+  let password = document.querySelector("[id=password]");
+  expect(username.value).toBe("");
+  expect(password.value).toBe("");
 
+  let usernameHelperText = document.querySelector("[id=username-helper-text]");
+  let passwordHelperText = document.querySelector("[id=password-helper-text]");
+  expect(usernameHelperText).not.toBeInTheDocument();
+  expect(passwordHelperText).not.toBeInTheDocument();
 
-    let username = document.querySelector("[id=username]");
-    let password = document.querySelector("[id=password]");
-    expect(username.value).toBe("");
-    expect(password.value).toBe("");
-
-    let usernameHelperText = document.querySelector("[id=username-helper-text]");
-    let passwordHelperText = document.querySelector("[id=password-helper-text]");
-    expect(usernameHelperText).not.toBeInTheDocument();
-    expect(passwordHelperText).not.toBeInTheDocument();
-
+  act(() => {
     fireEvent.change(username, { target: { value: "username" } });
     fireEvent.change(password, { target: { value: "password" } });
     fireEvent.click(getByTestId("sign-in-button"));
+  });
 
-    usernameHelperText = document.querySelector("[id=username-helper-text]");
-    passwordHelperText = document.querySelector("[id=password-helper-text]");
-    expect(usernameHelperText).not.toBeInTheDocument();
-    expect(passwordHelperText).not.toBeInTheDocument();
+  usernameHelperText = document.querySelector("[id=username-helper-text]");
+  passwordHelperText = document.querySelector("[id=password-helper-text]");
+  expect(usernameHelperText).not.toBeInTheDocument();
+  expect(passwordHelperText).not.toBeInTheDocument();
 
-    let errorAlert = getByTestId("login-error-alert");
-    expect(errorAlert).toBeInTheDocument();
-    expect(errorAlert.textContent).toBe("Incorrect username or password.")
-
-
-    // TODO: Ensure call and message displayed.
+  let errorAlert = await waitFor(() => getByTestId("login-error-alert"));
+  expect(errorAlert).toBeInTheDocument();
+  expect(errorAlert.textContent).toBe("Incorrect username or password.");
 });
 
 // test("Display error alert when response status is not 200 nor 401.", () => {
