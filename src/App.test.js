eed00d381a4c9a874176ac5e2620bebf8bad6c60
David Capilla <dcapillag@gmail.com>
2021-11-10 17:06:15 +0100

    dcg | 001 | Change formatting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-    render( < App / > );
-    const linkElement = screen.getByText("Welcome to Reflexiones de sofá");
-    expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Welcome to Reflexiones de sofá");
+  expect(linkElement).toBeInTheDocument();
 });
